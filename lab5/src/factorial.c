7e95f042286a1ecc43e6cb8ab831e53c8d08f188
MickeyShide <49620876+MickeyShide@users.noreply.github.com>
2024-04-25 10:14:56 +0000

    lab5: update factorial.c

--- lab5/src/factorial.c
+++ lab5/src/factorial.c
@@ -1,52 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 
-#define MAX_THREADS 100
-
-int factorial = 1;
-int mod;
-
+int k, pnum, mod;
 pthread_mutex_t mutex;
+unsigned long long result = 1;
 
-void* calculate_factorial(void* arg) {
-    int thread_id = (int)arg;
-    int start = thread_id * 1000;
-    int end = start + 1000;
-
-    for (int i = start; i < end; i++) {
-        factorial *= i;
-        factorial %= mod;
+void* factorial(void* arg) {
+    int start = *((int*)arg);
+    unsigned long long partial_result = 1;
+    
+    for (int i = start; i <= k; i += pnum) {
+        partial_result = (partial_result * i) % mod;
     }
-
+    
+    pthread_mutex_lock(&mutex);
+    result = (result * partial_result) % mod;
     pthread_mutex_unlock(&mutex);
-
+    
     return NULL;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        printf("Usage: ./program k pnum mod\n");
-        return 1;
+int main(int argc, char *argv[]) {
+    // -k --pnum --mod
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--k") == 0) {
+            k = atoi(argv[i+1]);
+        } else if (strncmp(argv[i], "--pnum", 7) == 0) {
+            pnum = atoi(argv[i] + 7);
+        } else if (strncmp(argv[i], "--mod", 6) == 0) {
+            mod = atoi(argv[i] + 6);
+        }
     }
 
-    int k = atoi(argv[1]);
-    int pnum = atoi(argv[2]);
-    mod = atoi(argv[3]);
-
     pthread_t threads[pnum];
+    int thread_args[pnum];
+
     pthread_mutex_init(&mutex, NULL);
 
     for (int i = 0; i < pnum; i++) {
-        pthread_mutex_lock(&mutex);
-        pthread_create(&threads[i], NULL, calculate_factorial, (void*)i);
+        thread_args[i] = i + 1;
+        pthread_create(&threads[i], NULL, factorial, &thread_args[i]);
     }
 
     for (int i = 0; i < pnum; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    printf("Factorial of %d mod %d: %d\n", k, mod, factorial);
+    printf("Factorial of %d mod %d: %llu\n", k, mod, result);
 
     pthread_mutex_destroy(&mutex);
 
