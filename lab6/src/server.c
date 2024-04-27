7ae5d8d2cea9e4f1b74e0fe6d860b798ada8d902
MickeyShide <49620876+MickeyShide@users.noreply.github.com>
2024-04-27 12:09:50 +0000

    lab6: add server.c

--- lab6/src/server.c
+++ lab6/src/server.c
@@ -13,6 +13,21 @@
 
 #include "pthread.h"
 
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
+uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
+  uint64_t result = 0;
+  a = a % mod;
+  while (b > 0) {
+    if (b % 2 == 1)
+      result = (result + a) % mod;
+    a = (a * 2) % mod;
+    b /= 2;
+  }
+
+  return result % mod;
+}
+
 struct FactorialArgs {
   uint64_t begin;
   uint64_t end;
@@ -35,8 +50,11 @@ uint64_t MultModulo(uint64_t a, uint64_t b, uint64_t mod) {
 uint64_t Factorial(const struct FactorialArgs *args) {
   uint64_t ans = 1;
 
-  // TODO: your code here
-
+  for (uint64_t i = args->begin; i <= args->end; i++)
+  {
+    ans *= i % args->mod;
+  }
+  ans %= args->mod;
   return ans;
 }
 
@@ -67,11 +85,19 @@ int main(int argc, char **argv) {
       switch (option_index) {
       case 0:
         port = atoi(optarg);
-        // TODO: your code here
+        if (port <=0)
+        {
+          printf("port is a positive number\n");
+          return 1;
+        }
         break;
       case 1:
         tnum = atoi(optarg);
-        // TODO: your code here
+        if (tnum <=0)
+        {
+          printf("tnum is a positive number\n");
+          return 1;
+        }
         break;
       default:
         printf("Index %d is out of options\n", option_index);
@@ -157,10 +183,11 @@ int main(int argc, char **argv) {
       fprintf(stdout, "Receive: %llu %llu %llu\n", begin, end, mod);
 
       struct FactorialArgs args[tnum];
+      uint64_t segments = (end-begin+1)/tnum;
       for (uint32_t i = 0; i < tnum; i++) {
-        // TODO: parallel somehow
-        args[i].begin = 1;
-        args[i].end = 1;
+        // parallelim
+        args[i].begin = begin + i * segments;
+        args[i].end = ((i != tnum - 1) ? begin + (i + 1) * segments : end);
         args[i].mod = mod;
 
         if (pthread_create(&threads[i], NULL, ThreadFactorial,
@@ -171,10 +198,13 @@ int main(int argc, char **argv) {
       }
 
       uint64_t total = 1;
-      for (uint32_t i = 0; i < tnum; i++) {
+      for (uint32_t i = 0; i < tnum; i++)
+      {
+        pthread_mutex_lock(&mut);
         uint64_t result = 0;
         pthread_join(threads[i], (void **)&result);
-        total = MultModulo(total, result, mod);
+        total =  multModulo(total, result, mod);
+        pthread_mutex_unlock(&mut);
       }
 
       printf("Total: %llu\n", total);
